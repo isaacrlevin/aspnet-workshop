199dc3bb9fa7e375a716cbe739de2534abaa72c5
Isaac Levin <isaac.r.levin@gmail.com>
2024-06-11 13:42:27 -0700

    Add save-point 4 start

---
+++ save-points/4-Add-Auth/ConferencePlanner/BackEnd/Data/SessionizeData.cs
@@ -0,0 +1,143 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace BackEnd.Data
+{
+    public class SessionizeData
+    {
+        [JsonPropertyName("sessions")]
+        public List<SessionizeSession> Sessions { get; set; }
+
+        [JsonPropertyName("speakers")]
+        public List<SessionizeSpeaker> Speakers { get; set; }
+
+        [JsonPropertyName("categories")]
+        public List<SessionizeCategory> Categories { get; set; }
+
+        [JsonPropertyName("rooms")]
+        public List<SessionizeRoom> Rooms { get; set; }
+    }
+
+    public class SessionizeSession
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("startsAt")]
+        public DateTime StartsAt { get; set; }
+
+        [JsonPropertyName("endsAt")]
+        public DateTime EndsAt { get; set; }
+
+        [JsonPropertyName("isServiceSession")]
+        public bool IsServiceSession { get; set; }
+
+        [JsonPropertyName("isPlenumSession")]
+        public bool IsPlenumSession { get; set; }
+
+        [JsonPropertyName("speakers")]
+        public List<string> Speakers { get; set; }
+
+        [JsonPropertyName("categoryItems")]
+        public List<int> CategoryItems { get; set; }
+
+        [JsonPropertyName("roomId")]
+        public int RoomId { get; set; }
+
+        [JsonPropertyName("liveUrl")]
+        public object LiveUrl { get; set; }
+
+        [JsonPropertyName("recordingUrl")]
+        public object RecordingUrl { get; set; }
+
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+
+        [JsonPropertyName("isInformed")]
+        public bool IsInformed { get; set; }
+
+        [JsonPropertyName("isConfirmed")]
+        public bool IsConfirmed { get; set; }
+    }
+
+    public class SessionizeSpeaker
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("bio")]
+        public string Bio { get; set; }
+
+        [JsonPropertyName("tagLine")]
+        public string TagLine { get; set; }
+
+        [JsonPropertyName("profilePicture")]
+        public string ProfilePicture { get; set; }
+
+        [JsonPropertyName("isTopSpeaker")]
+        public bool IsTopSpeaker { get; set; }
+
+        [JsonPropertyName("sessions")]
+        public List<int> Sessions { get; set; }
+
+        [JsonPropertyName("fullName")]
+        public string FullName { get; set; }
+
+        [JsonPropertyName("categoryItems")]
+        public List<int> CategoryItems { get; set; }
+    }
+
+    public class SessionizeCategory
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("items")]
+        public List<SessionizeItem> Items { get; set; }
+
+        [JsonPropertyName("sort")]
+        public int Sort { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+    }
+
+    public class SessionizeItem
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("sort")]
+        public int Sort { get; set; }
+    }
+
+    public class SessionizeRoom
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("sort")]
+        public int Sort { get; set; }
+    }
+}
---
+++ save-points/4-Add-Auth/ConferencePlanner/FrontEnd/Program.cs
@@ -0,0 +1,78 @@
+using FrontEnd.Components;
+using FrontEnd.Components.Account;
+using FrontEnd.Data;
+using FrontEnd.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FrontEnd
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddCascadingAuthenticationState();
+            builder.Services.AddScoped<IdentityUserAccessor>();
+            builder.Services.AddScoped<IdentityRedirectManager>();
+            builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+            builder.Services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+                })
+                .AddIdentityCookies();
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddSignInManager()
+                .AddDefaultTokenProviders();
+
+            builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+            builder.Services.AddHttpClient<IApiClient, ApiClient>(client =>
+            {
+                client.BaseAddress = new Uri(builder.Configuration["serviceUrl"]);
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            // Add additional endpoints required by the Identity /Account Razor components.
+            app.MapAdditionalIdentityEndpoints();
+
+            app.Run();
+        }
+    }
+}
