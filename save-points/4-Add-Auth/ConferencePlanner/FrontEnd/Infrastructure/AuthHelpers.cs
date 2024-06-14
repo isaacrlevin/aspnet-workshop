5bef05db8a62518b0b77f4e6ad76f6f050320590
Isaac Levin <isaac.r.levin@gmail.com>
2024-06-14 10:08:36 -0700

    Code Cleanup

--- save-points/4-Add-Auth/ConferencePlanner/FrontEnd/Infrastructure/AuthHelpers.cs
+++ save-points/4-Add-Auth/ConferencePlanner/FrontEnd/Infrastructure/AuthHelpers.cs
@@ -20,9 +20,6 @@ namespace System.Security.Claims
         public static bool IsAdmin(this ClaimsPrincipal principal) =>
             principal.HasClaim(AuthConstants.IsAdmin, AuthConstants.TrueValue);
 
-        public static void MakeAdmin(this ClaimsPrincipal principal) =>
-            principal.Identities.First().MakeAdmin();
-
         public static void MakeAdmin(this ClaimsIdentity identity) =>
             identity.AddClaim(new Claim(AuthConstants.IsAdmin, AuthConstants.TrueValue));
  }
--- save-points/5-Add-Personal-Agenda/ConferencePlanner/FrontEnd/Infrastructure/AuthHelpers.cs
+++ save-points/5-Add-Personal-Agenda/ConferencePlanner/FrontEnd/Infrastructure/AuthHelpers.cs
@@ -21,17 +21,11 @@ namespace System.Security.Claims
         public static bool IsAdmin(this ClaimsPrincipal principal) =>
             principal.HasClaim(AuthConstants.IsAdmin, AuthConstants.TrueValue);
 
-        public static void MakeAdmin(this ClaimsPrincipal principal) =>
-            principal.Identities.First().MakeAdmin();
-
         public static void MakeAdmin(this ClaimsIdentity identity) =>
             identity.AddClaim(new Claim(AuthConstants.IsAdmin, AuthConstants.TrueValue));
 
         public static bool IsAttendee(this ClaimsPrincipal principal) =>
             principal.HasClaim(AuthConstants.IsAttendee, AuthConstants.TrueValue);
-
-        public static void MakeAttendee(this ClaimsPrincipal principal) =>
-            principal.Identities.First().MakeAttendee();
 
         public static void MakeAttendee(this ClaimsIdentity identity) =>
             identity.AddClaim(new Claim(AuthConstants.IsAttendee, AuthConstants.TrueValue));
